c2b34007258ed0474f6f4279b9af88d3c33f32ca
Snow <polarpenguin@foxmail.com>
2019-05-26 21:46:06 +0800

    Change footer

--- footer/snow_zyf.php
+++ footer/snow_zyf.php
@@ -5,8 +5,10 @@ $signature_options = signature_get_options('signature_wp');
 <footer id="snow-footer" class="footer">
       <div class="row" style="margin-left: 100px;margin-right: 100px;">
           <article class="snow-footer-text col-xs-3 col-xs-offset-9">
-              <p style="letter-spacing: 3px;">数字媒体工作室</p>
-              <p>953732459@qq.com</p>
+<!--              <p style="letter-spacing: 3px;">数字媒体工作室</p>-->
+<!--              <p>953732459@qq.com</p>-->
+              <img alt="数字媒体工作室" title="数字媒体工作室" class="img-responsive" src="<?php echo esc_url(get_template_directory_uri()); ?>/_resources/snow_zyf_footer1.svg"/>
+              <a href="http://wpa.qq.com/msgrd?v=3&uin=806361380&site=qq&menu=yes"><img alt="953732459@qq.com" title="953732459@qq.com" class="img-responsive" src="<?php echo esc_url(get_template_directory_uri()); ?>/_resources/snow_zyf_footer2.svg"/></a>
           </article>
       </div>
 </footer>
